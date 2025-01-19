60c3e9f0a8b3ff5bcf5e17924902893eb71142eb
bryanhanggara <brynhnggra1903@gmail.com>
2025-01-19 20:56:46 +0700

    feat : home view for user

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,246 @@
+@extends('layouts.app')
+
+@section('title', 'General Dashboard')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet"
+        href="{{ asset('library/jqvmap/dist/jqvmap.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('library/summernote/dist/summernote-bs4.min.css') }}">
+    <style>
+        .card-square {
+            width: 200px;
+            height: 200px;
+            text-align: center;
+        }
+
+        .card-icon-circle {
+            width: 60px;
+            height: 60px;
+            border-radius: 50%;
+            margin: 10px auto;
+        }
+
+    </style>
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Dashboard</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-primary">
+                            <i class="fa fa-shopping-basket fa-2x text-white"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Laundrian Hari ini</h4>
+                            </div>
+                            <div class="card-body">
+                                10
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-success">
+                            <i class="fa fa-dollar-sign fa-2x text-white"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Pendapatan Hari ini</h4>
+                            </div>
+                            <div class="card-body">
+                                42
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="fa fa-spinner fa-2x text-white"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Sedang diproses</h4>
+                            </div>
+                            <div class="card-body">
+                                1,201
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="fa fa-file-invoice-dollar fa-2x text-white"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Belum dibayar</h4>
+                            </div>
+                            <div class="card-body">
+                                47
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1 card-square">
+                                <div class="bg-primary d-flex justify-content-center align-items-center card-icon-circle mt-5">
+                                    <i class="fa fa-bolt fa-2x text-white"></i>
+                                </div> 
+                                <div class="card-wrap">
+                                    <div class="card-body">
+                                        EXPRESS
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1 card-square">
+                                <div class="bg-primary d-flex justify-content-center align-items-center card-icon-circle mt-5">
+                                    <i class="fa fa-tshirt fa-2x text-white"></i>
+                                </div> 
+                                <div class="card-wrap">
+                                    <div class="card-body">
+                                        CUCI LIPAT
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1 card-square">
+                                <div class="bg-primary d-flex justify-content-center align-items-center card-icon-circle mt-5">
+                                    <i class="fa fa-star fa-2x text-white"></i>
+                                </div> 
+                                <div class="card-wrap">
+                                    <div class="card-body">
+                                        KOMPLIT
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1 card-square">
+                                <div class="bg-primary d-flex justify-content-center align-items-center card-icon-circle mt-5">
+                                    <i class="fa fa-tags fa-2x text-white"></i>
+                                </div> 
+                                <div class="card-wrap">
+                                    <div class="card-body">
+                                        SATUAN
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+                <div class="col-lg-4">
+                    <div class="card card-statistic-1 card-square">
+                        <div class="card-wrap">
+                            <div class="card-header bg-success d-flex justify-content-between align-items-center">
+                                <h2 class="text-white">Keuangan</h2>
+                                <i class="fa fa-wallet text-white fa-2x"></i> <!-- Ikon dompet -->
+                            </div>
+                            <div class="card-body text-center">
+                                <h1 class="mt-4">Rp. 1.000.000</h1> <!-- Ubah warna angka -->
+                                <p class="text-success">+ 800.000 (3%)</p> <!-- Ubah warna teks -->
+                            </div>
+                        </div>
+                    </div>                                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-md-12 col-12 col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Recent Activities</h4>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-unstyled list-unstyled-border">
+                                <li class="media">
+                                    <img class="rounded-circle mr-3"
+                                        width="50"
+                                        src="{{ asset('img/avatar/avatar-1.png') }}"
+                                        alt="avatar">
+                                    <div class="media-body">
+                                        <div class="text-primary float-right">Now</div>
+                                        <div class="media-title">Farhan A Mujib</div>
+                                        <span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla.
+                                            Nulla vel metus scelerisque ante sollicitudin.</span>
+                                    </div>
+                                </li>
+                                <li class="media">
+                                    <img class="rounded-circle mr-3"
+                                        width="50"
+                                        src="{{ asset('img/avatar/avatar-2.png') }}"
+                                        alt="avatar">
+                                    <div class="media-body">
+                                        <div class="float-right">12m</div>
+                                        <div class="media-title">Ujang Maman</div>
+                                        <span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla.
+                                            Nulla vel metus scelerisque ante sollicitudin.</span>
+                                    </div>
+                                </li>
+                                <li class="media">
+                                    <img class="rounded-circle mr-3"
+                                        width="50"
+                                        src="{{ asset('img/avatar/avatar-3.png') }}"
+                                        alt="avatar">
+                                    <div class="media-body">
+                                        <div class="float-right">17m</div>
+                                        <div class="media-title">Rizal Fakhri</div>
+                                        <span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla.
+                                            Nulla vel metus scelerisque ante sollicitudin.</span>
+                                    </div>
+                                </li>
+                                <li class="media">
+                                    <img class="rounded-circle mr-3"
+                                        width="50"
+                                        src="{{ asset('img/avatar/avatar-4.png') }}"
+                                        alt="avatar">
+                                    <div class="media-body">
+                                        <div class="float-right">21m</div>
+                                        <div class="media-title">Alfa Zulkarnain</div>
+                                        <span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla.
+                                            Nulla vel metus scelerisque ante sollicitudin.</span>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="pt-1 pb-1 text-center">
+                                <a href="#"
+                                    class="btn btn-primary btn-lg btn-round">
+                                    View All
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/simpleweather/jquery.simpleWeather.min.js') }}"></script>
+    <script src="{{ asset('library/chart.js/dist/Chart.min.js') }}"></script>
+    <script src="{{ asset('library/jqvmap/dist/jquery.vmap.min.js') }}"></script>
+    <script src="{{ asset('library/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+    <script src="{{ asset('library/summernote/dist/summernote-bs4.min.js') }}"></script>
+    <script src="{{ asset('library/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/index-0.js') }}"></script>
+@endpush
